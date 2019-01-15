a970e0b812f4e542d1f1f1fac471a453889c73a9
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-01-15 16:51:24 -0400

    static paths defined to deliver html

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\User;
+
+//the next header is set in order to allow React apps in development
+//to access the server's data. Remove it if unnecesary
+header('Access-Control-Allow-Origin:  localhost:3000');
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +23,19 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+/*
+ * Static routes you want to use in your react app. They'll return
+ * your HTML on ./public/react_index.html
+*/
+$static_router_routes = array(
+	"/dynamicUrl1",
+	"/dynamicUrl2",
+	"/dynamicUrl3",
+);
+foreach ($static_router_routes as $route) {
+	Route::get($route,function(){
+		return File::get('react_index.html');
+	});
+}
